597a292b7d51b34c4ff3d8eaea935a385bd87c51
danyjabban <112578130+danyjabban@users.noreply.github.com>
2022-12-09 13:37:44 -0500

    Add files via upload
    add simulation plot script

---
+++ logic/spi_plots.py
@@ -0,0 +1,37 @@
+"""This module plots the SPI distributions for the data and teams selected."""
+
+import pandas as pd
+import numpy as np
+import altair as alt
+
+
+def spi_plots(spi_dist_df):
+    """Plot the distributions for the two teams selected, with the higher ranked team in green and the lower in red.
+
+    Args:
+        spi_dist_df (pd.DataFrame): DataFrame containing the distributions for each team with this structure:
+            team1 (float): SPI rank for team 1
+            team2 (float): SPI rank for team 2
+            winner (str): Name of the winning team of that simulation when compared
+
+    Returns:
+        spi_plots (alt.Chart): Altair chart object containing the plots for the two teams selected"""
+
+    # rename the columns to team1, team2, winner
+    spi_dist_df = spi_dist_df.rename(
+        columns={spi_dist_df.columns[0]: "team1", spi_dist_df.columns[1]: "team2"}
+    )
+
+    # Create the plots
+    spi_plot = (
+        alt.Chart(spi_dist_df)
+        .transform_fold(["team1", "team2"], as_=["Experiment", "Measurement"])
+        .mark_bar(opacity=0.2, binSpacing=0)
+        .encode(
+            alt.X("Measurement:Q", bin=alt.Bin(maxbins=100)),
+            alt.Y("count()", stack=None),
+            alt.Color("Experiment:N"),
+        )
+    )
+
+    return spi_plot
